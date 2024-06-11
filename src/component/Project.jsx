fa7e28d22d20902197cd6dc63d97819b9ba729eb
halimamrin312 <130393111+halimamrin312@users.noreply.github.com>
2024-06-11 09:15:28 +0700

    Fit Project, Againn

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -11,17 +11,17 @@ function Project() {
                 <p>MY PROJECT</p>
             </div>
             <div className='flex flex-wrap justify-between gap-10'>
-<<<<<<< HEAD
+{/* <<<<<<< HEAD */}
                 <CustomLink src={img1} href="https://github.com/halimamrin312/manajementevent">MANAJEMEN EVENT</CustomLink>
                 <CustomLink src={img2} href="https://github.com/halimamrin312/portofolio">WEBSITE PORTOFOLIO</CustomLink>
                 <CustomLink src={img3} href="https://github.com/halimamrin312/RHD---Project">RHD</CustomLink>
                 <CustomLink src={img4} href="https://www.figma.com/proto/DYJmBTw9UhONRjLIFXLDkR/DuckGear?node-id=4-2&t=FghOWKRnyk4IFTCT-1">Duck-Gear</CustomLink>
-=======
-                <CustomLink src="../../public/assets/image1.png" href="https://github.com/halimamrin312/manajementevent">MANAJEMEN EVENT</CustomLink>
+{/* ======= */}
+                {/* <CustomLink src="../../public/assets/image1.png" href="https://github.com/halimamrin312/manajementevent">MANAJEMEN EVENT</CustomLink>
                 <CustomLink src="../../public/assets/image2.png" href="https://github.com/halimamrin312/portofolio">WEBSITE PORTOFOLIO</CustomLink>
                 <CustomLink src="../../public/assets/image3.png" href="https://github.com/halimamrin312/RHD---Project">RHD</CustomLink>
-                <CustomLink src="../../public/assets/image4.png" href="https://www.figma.com/proto/DYJmBTw9UhONRjLIFXLDkR/DuckGear?node-id=4-2&t=FghOWKRnyk4IFTCT-1">Duck-Gear</CustomLink>
->>>>>>> 54738e3d58b32ffe50690746a92ae7c8576e16ad
+                <CustomLink src="../../public/assets/image4.png" href="https://www.figma.com/proto/DYJmBTw9UhONRjLIFXLDkR/DuckGear?node-id=4-2&t=FghOWKRnyk4IFTCT-1">Duck-Gear</CustomLink> */}
+{/* >>>>>>> 54738e3d58b32ffe50690746a92ae7c8576e16ad */}
             </div>
 
 
